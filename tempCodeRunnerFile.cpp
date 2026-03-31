79dbf65668934629bd955ef4b26cf487c65db2b4
Naseer <feznaseer777@gmail.com>
2026-03-31 21:13:16 +0530

    Kadane's Algo

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int Maxiumsum(int arr[], int n)
+{
+int sum=arr[0];
+
+for (int i = 0; i < n; i++)
+{
+        int actsu=arr[i];
+    for (int j = i+1; j < n; j++)
+    {
+        actsu+=arr[j];
+
+
+
+    }
+    
+
+sum=std::max(actsu,sum);
+
+
+}
+
+
+
+
+return sum;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int arr[] = {-1,-3,4,-1,-2,1,5,-3};
+    cout << "[" << Maxiumsum(arr, sizeof(arr) / sizeof(arr[0])) << "]";
+
+    return 0;
+}
