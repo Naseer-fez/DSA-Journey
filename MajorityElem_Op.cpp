cc9d72c5843360a7e7adc6a14894fba63aad8b9b
Naseer <feznaseer777@gmail.com>
2026-03-31 20:45:08 +0530

    Moore Voting Algorithim

--- MajorityElem_Op.cpp
+++ MajorityElem_Op.cpp
@@ -4,38 +4,43 @@ using namespace std;
 
 int majorityelemnt(int arr[], int n)
 {
-
-    std::unordered_map<int, int> map;
-
-    for (int i = 0; i < n; i++)
+    int element = arr[0];
+    int count = 1;
+    for (int i = 1; i < n; i++)
     {
 
-        map[arr[i]]++;
-    }
-    int max = 0;
-    int elemnt = 0;
-    for (auto i = map.begin(); i != map.end(); i++)
-    {
-        if (i->second > max)
+        if (element == arr[i])
         {
-            max = i->second;
-            elemnt = i->first;
+            count++;
+        }
+        else
+        {
+            count--;
+        }
+        if (count == 0)
+        {
+
+            element = arr[i];
+            count = 1;
         }
     }
-    if(!(max>n/2))return 0;
+    int check = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == element)
+            check++;
+    }
 
-
-
-    cout << "" << elemnt << "->";
-
-    return max;
+    if (check > n / 2)
+        return element;
+    return 0;
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int arr[] = {1, 2, 2, 3, 3, 2, 2, 2, 3, 1};
+    int arr[] = {7, 7, 5, 7, 5, 1, 5, 7, 5, 5, 7, 7, 5, 5, 5, 5};
     cout << "[" << majorityelemnt(arr, sizeof(arr) / sizeof(arr[0])) << "]";
 
     return 0;
