08a521170ea9b4887f3cac010554ad70ca273efb
Naseer <feznaseer777@gmail.com>
2026-08-11 21:03:48 +0530

    Double loop method

---
+++ BinarrySearch/mindaystoMboq_brute.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int binary(int arr[], int n, int boq, int adjacent)
+{
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int currentelem = arr[i];
+        int adj = 0;
+        int boquo = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[j] <= currentelem)
+            {
+                adj++;
+
+                if (adj == adjacent)
+                {
+                    adj = 0;
+                    boquo++;
+                    if (boquo == boq)
+                    {
+                        return currentelem;
+                    }
+                }
+            }else{
+                adj==0;
+            }
+
+        }
+    }
+    return -9;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int arr[] = {7, 7, 7, 7, 13, 11, 12, 7};
+    int n = sizeof(arr) / sizeof(int);
+
+    int value = binary(arr, n, 2, 3);
+    cout << value;
+}
